53faa59b083ef78a70716d8e409804468a8cb20c
feoolga <kirianova_ov@mail.ru>
2023-10-02 12:34:09 +0300

    edit any blocks

--- js/script.js
+++ js/script.js
@@ -2,8 +2,42 @@ const loader = document.querySelector('#loader')
 let paddingSpoiler = 10
 
 window.onload = function(){
+
 	loader.remove()
+
+/* обсервер  /---/---/---/---/---/---/---/---/---/--- */
+
+	let options = {
+		root: null,
+		rootMargin: "5px",
+		threshold: 0.5,
+	};
+
+	console.log(555);
+
+	const observer = new IntersectionObserver((entries, observer) => {
+		// для каждой записи-целевого элемента
+		entries.forEach(entry => {
+			// если элемент является наблюдаемым
+			if (entry.isIntersecting) {
+				const elemAnim = entry.target
+				// меняем класс
+				// entry.isIntersecting ? entry.target.classList.replace('active', 'activeBack') : entry.target.classList.replace('activeBack', 'active')
+				elemAnim.classList.add('active')
+				// прекращаем наблюдение
+				observer.unobserve(elemAnim)
+			}
+		})
+	}, options)
+
+	// с помощью цикла следим за всеми классами anim на странице
+	const animations = document.querySelectorAll('.anim')
+	animations.forEach(animation => {
+		observer.observe(animation)
+	})
 }
+
+/* аккордеон-спойлер  /---/---/---/---/---/---/---/---/---/--- */
 
 document.querySelectorAll(".spoiler__title").forEach((elem) => {
 	// console.log(elem);
@@ -17,6 +51,7 @@ document.querySelectorAll(".spoiler__title").forEach((elem) => {
 				elem.style.paddingBottom = '0'
 				elem.style.maxHeight = null
 				elem.classList.remove('open')
+				elem.previousElementSibling.classList.remove('open')
 			})
 		} else{
 			document.querySelectorAll(".spoiler__text").forEach((elem) => {
@@ -24,26 +59,63 @@ document.querySelectorAll(".spoiler__title").forEach((elem) => {
 				elem.style.paddingBottom = '0'
 				elem.style.maxHeight = null
 				elem.classList.remove('open')
+				elem.previousElementSibling.classList.remove('open')
 			})
 			text.style.paddingTop = paddingSpoiler + "px"
 			text.style.paddingBottom = paddingSpoiler + "px"
 			text.style.maxHeight = text.scrollHeight + (paddingSpoiler*2) + "px"  //Переменная?
 			elem.nextElementSibling.classList.add('open')
+			elem.classList.add('open')
 		}
 	})
 })
 
-const swiper = new Swiper('.swiper', {
+/* слайдер свайпер  /---/---/---/---/---/---/---/---/---/--- */
 
-	direction: 'horizontal',
-	loop: true,
-  
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  
-	scrollbar: {
-	  el: '.swiper-scrollbar',
+const swiper = new Swiper('.swiper', {
+	effect: 'coverflow',
+	coverflowEffect: {
+	  rotate: 30,
+	//   slideShadows: false,
+	  depth: 100,
+	  modifier: 1,
+	  slideShadows: true,
 	},
   });
+
+// const swiper = new Swiper('.swiper', {
+// 	effect: `cards`,
+// 	cardsEffect: {
+// 		perSlideOffset: 5,
+// 		perSlideRotate:2,
+// 		rotate: true,
+// 		slideShadows:true,
+// 	},
+//   });
+
+/* стрелочка вверх  /---/---/---/---/---/---/---/---/---/--- */
+
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+      this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+      this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+      window.addEventListener('scroll', () => {
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        scrollY > 400 ? this.show() : this.hide();
+      });
+      document.querySelector('.btn-up').onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  }
+  
+  btnUp.addEventListener();
